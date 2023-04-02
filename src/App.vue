46bac578ef2a91b9c2678ac9c644bf17e59f38ec
CatzillaOrz <catzillaorz@outlook.com>
2023-04-02 14:48:07 +0800

    feat: scss | grid

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+@import './sass/main';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +25,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  img {
+    width: 40px;
+  }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <section>
+      <div class="grid-test">
+        <div class="row">
+          <div class="col-1-of-2">col 1 of 2</div>
+          <div class="col-1-of-2">col 1 of 2</div>
+        </div>
+
+        <div class="row">
+          <div class="col-1-of-3">col 1 of 3</div>
+          <div class="col-1-of-3">col 1 of 3</div>
+          <div class="col-1-of-3">col 1 of 3</div>
+        </div>
+
+        <div class="row">
+          <div class="col-1-of-3">col 1 of 3</div>
+          <div class="col-2-of-3">col 2 of 3</div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1-of-4">col 1 of 4</div>
+        <div class="col-1-of-4">col 1 of 4</div>
+        <div class="col-1-of-4">col 1 of 4</div>
+        <div class="col-1-of-4">col 1 of 4</div>
+      </div>
+
+      <div class="row">
+        <div class="col-1-of-4">col 1 of 4</div>
+        <div class="col-1-of-4">col 1 of 4</div>
+        <div class="col-2-of-4">col 2 of 4</div>
+      </div>
+
+      <div class="row">
+        <div class="col-1-of-4">col 1 of 4</div>
+        <div class="col-3-of-4">col 3 of 4</div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -15,10 +52,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-.hello{
+@import '../sass/main';
+.hello {
   max-width: 14rem;
 }
-h1{
+h1 {
   font-size: 14px;
 }
 </style>
