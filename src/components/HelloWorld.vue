523b63f54088599d56dbf5b27dbe611597f60924
CatzillaOrz <catzillaorz@outlook.com>
2023-04-02 14:12:13 +0800

    feat: scss variables | grid var

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,19 +14,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+.hello{
+  max-width: 14rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+h1{
+  font-size: 14px;
 }
 </style>
